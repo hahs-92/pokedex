2a7b56b4e4039a367b764097e5146613f8c0eeeb
Alex Hernandez <alexandre_2420@outlook.com>
2021-06-16 18:37:31 -0500

    header- search component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,26 @@
+//ESTILOS
+import styles from '../styles/Home.module.css'
+
 const Home = () => {
     return(
-        <h1>Home</h1>
+        <section className={ styles.Home }>
+            <section className={ styles.Header }>
+                <header className={ styles.Header_wrapper }>
+                    <h1>Pokedex</h1>
+                </header>
+            </section>
+
+            <section className={ styles.Search }>
+                <div className={ styles.Search_wrapper }>
+                    <input className={ styles.Input } type="text" placeholder='Search by name' />
+                    <input className={ styles.Button } type="button" value='Search' />
+                </div>
+            </section>
+
+            <main className={ styles.Main }>
+
+            </main>
+        </section>
     )
 }
 
