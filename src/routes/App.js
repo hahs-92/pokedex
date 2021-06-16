40f3ca2faf5deaff54ef6fb8d8e393fbb9315602
Alex Hernandez <alexandre_2420@outlook.com>
2021-06-16 17:26:19 -0500

    Project setup

---
+++ src/routes/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+//PAGES
+import Home from '../pages/Home'
+import Detail from '../pages/Detail'
+//ESTILOS
+import '../styles/Global.css'
+
+const App = () => {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={ Home }/>
+                <Route exact path='/Pokemon/:id' component={ Detail } />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default App
