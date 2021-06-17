eac8e1043d55aa24837c6c64851c3f6ba7993814
Alex Hernandez <alexandre_2420@outlook.com>
2021-06-17 10:25:08 -0500

    getdata call api

--- src/components/CardMain.js
+++ src/components/CardMain.js
@@ -1,19 +1,45 @@
-//ASSETS
-import imgDefault from '../assets/1.svg'
+import { useEffect, useState } from 'react'
 //ESTILOS
 import styles from '../styles/components/CardMain.module.css'
 
-const CardMain = () => {
-    return(
-        <article className={ styles.CardMain }>
-            <section className={ styles.Imagen }>
-                <img src={ imgDefault } alt="pokemon" />
-            </section>
+const CardMain = ({ name }) => {
+    const [ imgPokemon, setImgPokemon ] = useState('')
+    const BASEURL = "https://pokeapi.co/api/v2/"
 
-            <section className={ styles.Title }>
-                <h2>Name Pokemon</h2>
-            </section>
-        </article>
+    const getImgPokemon = async() => {
+        try {     
+            const data = await fetch(`${ BASEURL }pokemon/${ name }`)
+            const response = await data.json()
+            setImgPokemon( response.sprites.other["official-artwork"].front_default)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    useEffect(() => {
+        // eslint-disable-next-line
+        getImgPokemon()
+    }, [])
+
+    return(
+        <>
+            {
+                imgPokemon 
+                ?
+                <article className={ styles.CardMain }>
+                    <section className={ styles.Imagen }>
+                        <img src={ imgPokemon } alt={ `pokemon ${ name }`} />
+                    </section>
+
+                    <section className={ styles.Title }>
+                        <h2>{ name }</h2>
+                    </section>
+                </article>
+                :
+                <h1>Not Results</h1>
+            }
+        </>
+        
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 //ESTILOS
 import styles from '../styles/Home.module.css'
 //COMPONENTS
@@ -5,26 +6,67 @@ import Header from '../components/Header'
 import CardMain from '../components/CardMain'
 
 const Home = () => {
+    const BASEURL = "https://pokeapi.co/api/v2/"
+    const LIMIT = 10
+    const [ pokemon, setPokemon ] = useState([])
+    const [ page, setPage ] = useState(0)
+    const [ filter, setFilter ] = useState('')
+
+    const getData = async() => {
+        try {
+            const data = await fetch(`${ BASEURL }pokemon?limit=${ LIMIT }&offset=${ page * LIMIT}`)
+            const response = await data.json()
+            const newData = [...pokemon, ...response.results]
+            setPokemon(newData)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    const handleNextPage = () => {
+        setPage(page + 1 )
+    }
+
+    const handleOnChange = (e) => {
+        setFilter(e.target.value)
+    }
+
+    const handleSearch = () => {
+        if(!filter) return false
+        setPokemon([{ name: filter }])
+    }
+
+    useEffect(() => {
+        // eslint-disable-next-line
+        getData()
+    },[page])
+
     return(
         <section className={ styles.Home }>
             <Header />
 
             <section className={ styles.Search }>
                 <div className={ styles.Search_wrapper }>
-                    <input className={ styles.Input } type="text" placeholder='Search by name' />
-                    <input className={ styles.Button } type="button" value='Search' />
+                    <input className={ styles.Input } onChange={ handleOnChange } type="text" placeholder='Search by name' />
+                    <input className={ styles.Button } onClick={ handleSearch } type="button" value='Search' />
                 </div>
             </section>
 
             <main className={ styles.Main }>
                 <section className={ styles.Main_wrapper }>
-                    <CardMain />
-                    <CardMain />
-                    <CardMain />
-                    <CardMain />
-                    <CardMain />
-                    <CardMain />
-                    <CardMain />
+                    {
+                        pokemon.length > 0 &&
+                            pokemon.map(item => (
+                                <CardMain key={ item.name } name={ item.name }/>
+                            ))
+                    }
+                </section>
+
+                <section className={ styles.Main_NextPage }>
+                    {
+                        pokemon.length > 1 &&
+                            <button type='button' onClick={ handleNextPage }>View more</button>
+                    }
                 </section>
             </main>
         </section>
